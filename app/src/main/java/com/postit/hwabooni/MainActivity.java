c72dabb0391a8dd6113b8bebf29888179ec975b0
jibminJung <jibminjung@gmail.com>
2022-05-09 19:16:31 +0900

    initial commit

---
+++ app/src/main/java/com/postit/hwabooni/MainActivity.java
@@ -0,0 +1,14 @@
+package com.postit.hwabooni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
